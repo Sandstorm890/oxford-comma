a735853948228253e539b142c52f9403bef4232c
zaynegoering <zaynegoering@gmail.com>
2020-10-21 11:53:55 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.join()
+    elsif array.size == 2
+        x = array[0..(array.size - 2)]
+        string = x.join(", ")
+        string + " and #{array.pop}"
+    else
+        x = array[0..(array.size - 2)]
+        string = x.join(", ")
+        string + ", and #{array.pop}"
+    end
 end
