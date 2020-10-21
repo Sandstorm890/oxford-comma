c437fec52a2ccf9331a4d028cd45c708e574a5f2
zaynegoering <zaynegoering@gmail.com>
2020-10-21 11:51:31 -0700

    commit tree cd26178247386f58c0122c6ba490b9c500ef06fb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
+# names = ["Jeff", "Steve", "Carl", "Norman"]
 
+def oxford_comma(array)
+    if array.size == 1
+        array.join()
+    elsif array.size == 2
+        x = array[0..(array.size - 2)]
+        string = x.join(", ")
+        string + " and #{array.pop}"
+    else
+        x = array[0..(array.size - 2)]
+        string = x.join(", ")
+        string + ", and #{array.pop}"
+    end
 end
+
+
+# puts oxford_comma(names)
